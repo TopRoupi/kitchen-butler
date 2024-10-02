4f8dde215bd2729f6e027447e615776f872c611c
Vincenzo Fehring <vinfehring@gmail.com>
2024-10-02 01:16:08 -0500

    Updated pages, created landing page

--- kitchen_butler/pages/views.py
+++ kitchen_butler/pages/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-@login_required
 def home(request):
-    return render(request, 'pages/home.html')
+    if request.user.is_authenticated:
+        return render(request, 'pages/home.html')
+    else:
+        return render(request, 'pages/landing.html')
