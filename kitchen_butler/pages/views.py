e603b68c99a4ec542e1cbc35fe24e795b539a434
Vincenzo Fehring <vinfehring@gmail.com>
2024-10-02 01:46:02 -0500

    Starter design for dashboard

--- kitchen_butler/pages/views.py
+++ kitchen_butler/pages/views.py
@@ -3,6 +3,14 @@ from django.contrib.auth.decorators import login_required
 
 def home(request):
     if request.user.is_authenticated:
-        return render(request, 'pages/home.html')
+        return render(request, 'pages/dashboard.html')
     else:
         return render(request, 'pages/landing.html')
+    
+@login_required
+def dashboard(request):
+    # You can add context data here as needed
+    context = {
+        'user': request.user,
+    }
+    return render(request, 'pages/dashboard.html', context)
